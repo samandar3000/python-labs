595a75768ca38ece4ec875453511c7f8a5c4f9a4
samandar3000 <115165854+samandar3000@users.noreply.github.com>
2022-12-08 15:10:00 +0300

    Update second.py

--- lab2/second.py
+++ lab2/second.py
@@ -1,1 +1,16 @@
 
+
+while True:
+    a = input("Введите номер: ")
+    b = input("Введите другой номер:  ")
+    
+    try:
+        a = int(a); b = int(b)
+        minimum = min(a,b)
+        print(minimum)
+        break
+        
+    except  ValueError:
+        print("введите правильный выбор")
+        
+    
