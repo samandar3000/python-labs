80aa11ed4812c47c2831c4178dff27cd7c1ee290
samandar3000 <115165854+samandar3000@users.noreply.github.com>
2022-12-08 15:11:09 +0300

    Update third.py

--- lab2/third.py
+++ lab2/third.py
@@ -1,1 +1,17 @@
 
+
+while True:
+    a = input("Введите номер: ")
+    b = input("Введите другой номер:  ")
+    
+    try:
+        a = int(a); b = int(b)
+        maximum = max(a,b)
+        print()
+        print(f' найбольшее число равно {maximum}')
+        break
+        
+    except  ValueError:
+        print("введите правильный выбор")
+        
+    
